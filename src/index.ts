dc15f08cbf7257a4b71f15d43e7582873a5aaf86
Harrison Black <harrison@macbook.local>
2024-03-20 18:00:11 +1100

    basic html serve

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+  port: 3000,
+  fetch(req) {
+    return new Response(Bun.file("src/index.html"));
+  },
+});
+
+console.log(`Listening on http://localhost:${server.port} ...`);
