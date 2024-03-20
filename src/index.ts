05a728138698157f0ac9d72d285854e1d570792b
Harrison Black <harrison@macbook.local>
2024-03-20 18:54:25 +1100

    port argument

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,27 @@
+import { parseArgs } from "util";
+
+const { values, positionals } = parseArgs({
+  args: Bun.argv,
+  options: {
+    port: {
+      type: 'string'
+    },
+  },
+  stric: true,
+  allowPositionals: true,
+});
+
+let port;
+if (values.port != null){
+  port = values.port;
+} else {port = 3000}
+
+
 const server = Bun.serve({
-  port: 3000,
+  port: port,
   fetch(req) {
     return new Response(Bun.file("src/index.html"));
   },
 });
 
-console.log(`Listening on http://localhost:${server.port} ...`);
+console.log(`Listening on http://localhost:${server.port}...`);
